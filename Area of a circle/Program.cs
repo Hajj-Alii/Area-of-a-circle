db93364cc22b2c1cca3c395e3334863039d0f7b1
Hajj Ali <Hajj Ali@HAJJ_ALII>
2022-02-03 17:44:28 +0330

    Get and Calculate Area

--- Area of a circle/Program.cs
+++ Area of a circle/Program.cs
@@ -10,6 +10,13 @@ namespace Area_of_a_circle
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter Radius : ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine($"Area : {Math.PI * Math.Pow(Convert.ToDouble(str), 2)}");
+
+            Console.WriteLine("\n\nPress Enter To Exit");
+            Console.ReadLine();
         }
     }
 }
